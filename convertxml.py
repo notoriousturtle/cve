ab06f6cb3fa47efd560d7957f7bcb1d6c105f18f
spara <sophia@jetpac.com>
2015-02-20 20:16:22 -0600

    update

--- convertxml.py
+++ convertxml.py
@@ -1,6 +1,7 @@
 import json
 import xmltodict
-from operator import getitem
+import urllib
+import tempfile
 
 def convert(xml_file, xml_attribs=True):
     with open(xml_file, "rb") as f:    # notice the "rb" mode
@@ -9,11 +10,34 @@ def convert(xml_file, xml_attribs=True):
         #return json.dumps(d, indent=4)
         return d
 
-files = ["./data/nvdcve-2.0-2002.xml", "./data/nvdcve-2.0-2003.xml", "./data/nvdcve-2.0-2004.xml", "./data/nvdcve-2.0-2005.xml", "./data/nvdcve-2.0-2006.xml", "./data/nvdcve-2.0-2007.xml", "./data/nvdcve-2.0-2008.xml", "./data/nvdcve-2.0-2009.xml", "./data/nvdcve-2.0-2010.xml", "./data/nvdcve-2.0-2011.xml", "./data/nvdcve-2.0-2012.xml", "./data/nvdcve-2.0-2013.xml", "./data/nvdcve-2.0-2014.xml", "./data/nvdcve-2.0-2015.xml"]
+def download(files, base_url):
+	for f in files:
+		localfile = urllib.URLopener()
+		localfile.retrieve(''.join([base_url,f]),f)
+
+def deletexml(files):
+	for f in files:
+		os.remove(f)
+
+def showkeys(data):
+	k = []
+	for key in data.keys():
+		k.append(key)
+	print k
+
+
+
+files = ["nvdcve-2.0-2002.xml", "nvdcve-2.0-2003.xml", "nvdcve-2.0-2004.xml", "nvdcve-2.0-2005.xml", "nvdcve-2.0-2006.xml", "nvdcve-2.0-2007.xml", "nvdcve-2.0-2008.xml", "nvdcve-2.0-2009.xml", "nvdcve-2.0-2010.xml", "nvdcve-2.0-2011.xml", "nvdcve-2.0-2012.xml", "nvdcve-2.0-2013.xml", "nvdcve-2.0-2014.xml", "nvdcve-2.0-2015.xml"]
+#files = ["nvdcve-2.0-2002.xml"]
+base_url = "https://nvd.nist.gov/feeds/xml/cve/"
+
+# download(files, base_url)
+
 
 for f in files:
 	doc = convert(f)
 	for i, entry in enumerate(d['nvd']['entry'] for d in doc): 
 		for j, cve in enumerate(c for c in entry):
     			print json.dumps(cve)
+    			#showkeys(cve)
 
